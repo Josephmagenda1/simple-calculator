44ad64635cb5aae88977191918edd147cea252a9
Josephmagenda1 <101411539+Josephmagenda1@users.noreply.github.com>
2022-11-22 14:08:44 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,37 @@
+// Program to create a simple calculator
+#include <stdio.h>
+
+int main() {
+    char operation;
+    double n1, n2;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operation);
+    printf("Enter an operand: ");
+    scanf("%lf %lf",&n1, &n2);
+
+    switch(operation)
+    {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf",n1, n2, n1+n2);
+            break;
+
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf",n1, n2, n1-n2);
+            break;
+
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf",n1, n2, n1*n2);
+            break;
+
+        case '/':
+            printf("%.2lf / %.2lf = %.2lf",n1, n2, n1/n2);
+            break;
+
+        // operator doesn't match any case constant +, -, *, /
+        default:
+            printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
